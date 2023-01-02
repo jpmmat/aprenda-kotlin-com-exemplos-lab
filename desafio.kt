e9f963faf5328fde217bbf4bde695f2bc19e664b
jpmmat <97574443+jpmmat@users.noreply.github.com>
2023-01-02 01:05:59 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,9 +1,8 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-// criar uma classe para usuario com alguns dados
-class Usuario(val nome: String, val email: String, val senha: String, val nivelConhecimento: Nivel)
 
+class Usuario(val nome: String, val email: String, val senha: String, val nivelConhecimento: Nivel)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -11,38 +10,43 @@ data class Formacao(val nome: String, val nivelDificuldade: Nivel, var conteudos
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-	 //inserindo novos inscrintos pela funcao matricular
         //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
         inscritos.add(usuario)
+        
     }
     
     fun getInscritosOrdenadosPorNivelConhecimento(): List<Usuario> {
-	    // ordenar os incrintos por nivel
         return inscritos.sortedBy { it.nivelConhecimento }
     }
+    fun usuarioEstaInscrito(usuario: Usuario): Boolean {
+    return usuario in inscritos
+    
 }
 
-
+}
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-    // os conteudos progamados
+    
+    
+//    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    val usuario1 = Usuario("João", "joao@email.com", "senha123", Nivel.INTERMEDIARIO)
+    val usuario2 = Usuario("Maria", "maria@email.com", "senha456", Nivel.DIFICIL)
+    val usuario3 = Usuario("Pedro", "pedro@email.com", "senha789", Nivel.BASICO)
+
+    
+//    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
     val conteudo1 = ConteudoEducacional("Introdução à programação")
     val conteudo2 = ConteudoEducacional("Estruturas de dados", 90)
     val conteudo3 = ConteudoEducacional("Algoritmos de ordenação")
     
-    val formacao1 = Formacao("Curso de programação", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2, conteudo3))
+    val formacao1 = Formacao("$conteudo1", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2, conteudo3))
     
-    val inscritosOrdenados = formacao1.getInscritosOrdenadosPorNivelConhecimento()
-    
-    val usuario1 = Usuario("João", "joao@email.com", "senha123", Nivel.INTERMEDIARIO)
-    val usuario2 = Usuario("Maria", "maria@email.com", "senha456",  Nivel.DIFICIL)
-    val usuario3 = Usuario("Pedro", "pedro@email.com", "senha789",  Nivel.BASICO)
-    
-	
-    
-	formacao1.matricular(usuario1)
+    formacao1.matricular(usuario1)
 	formacao1.matricular(usuario2)
 	formacao1.matricular(usuario3)
+  
+  	println("Usuários inscritos na formação ${formacao1.nome}:")
+    for (usuario in formacao1.inscritos) {
+        println(usuario.nome)
+    }
     
 }
